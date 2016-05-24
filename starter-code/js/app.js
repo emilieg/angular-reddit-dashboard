c7eaf8c69fc6ad3642673bb54d11d299afd3b5ac
emilieg <emilie.gerber@hotmail.com>
2016-05-24 16:11:52 -0700

    API is working

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,3 +1,43 @@
 /* put js code here */
+var redditApp = angular.module('RedditApp', []);
+redditApp.controller('SearchCtrl', ['$scope', '$http', function($scope, $http){
+  $scope.searchTerm ='';
+  $scope.data= {
+    title: '',
+    img: '',
+    permalink: '',
+    score: ''
+  };
+
+
+
+
+  $scope.search = function(){
+    var req = {
+      url: "https://www.reddit.com/search.json",
+      method: 'GET',
+      params: {
+        q: $scope.searchTerm
+      }
+    }
+
+    $http(req).then(function success(res){
+      var searchData = res.data;
+      $scope.data.title = searchData.data.children[0].data.title;
+      $scope.data.img = searchData.data.children[0].data.url;
+      $scope.data.permalink = searchData.data.children[0].data.permalink;
+      $scope.data.score = searchData.data.children[0].data.score;
+
+    }, function error(res){
+      console.log(res);
+    })
+  }
+
+
+
+
+
+
+}])
 
 console.log('this file is running.');
