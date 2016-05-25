4ad13e7c1870af223cdea1e7f82a53bf14a2f108
emilieg <emilie.gerber@hotmail.com>
2016-05-25 09:27:37 -0700

    localStorage, add delete and research

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,40 +2,54 @@
 var redditApp = angular.module('RedditApp', []);
 redditApp.controller('SearchCtrl', ['$scope', '$http', function($scope, $http){
   $scope.searchTerm ='';
-  $scope.data= {
-    title: '',
-    img: '',
-    permalink: '',
-    score: ''
-  };
-
+  $scope.dat = [];
+  $scope.history = [];//array to store searchTerms
+  if (localStorage.getItem('term')) {
+    $scope.history = JSON.parse(localStorage.getItem('term'))
+  }
 
 
 
   $scope.search = function(){
-    var req = {
+    $scope.research($scope.searchTerm);
+  };
+
+  $scope.research = function(term){
+  
+        var req = {
       url: "https://www.reddit.com/search.json",
       method: 'GET',
       params: {
-        q: $scope.searchTerm
+        q: term
       }
     }
 
     $http(req).then(function success(res){
       var searchData = res.data;
-      $scope.data.title = searchData.data.children[0].data.title;
-      $scope.data.img = searchData.data.children[0].data.url;
-      $scope.data.permalink = searchData.data.children[0].data.permalink;
-      $scope.data.score = searchData.data.children[0].data.score;
+      console.log(searchData);
+      //loop over children array and grab 
+      var arr = searchData.data.children;
+      for (i = 0; i < arr.length; i++){
+          $scope.dat[i]=arr[i].data;
+
+      }
+        if ($scope.history.indexOf(term) === -1) {
+            $scope.history.push(term);
+            console.log("adding term to history : "+term)
+            localStorage.setItem('term',JSON.stringify($scope.history));
+        }
+
 
     }, function error(res){
       console.log(res);
     })
   }
 
-
-
-
+  $scope.delete = function(index){
+    localStorage.removeItem(index);
+     $scope.history.splice(index, 1);
+  }
+ 
 
 
 }])
